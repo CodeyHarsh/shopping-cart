b20c8ff051bbf31e886bbb7f9fe4e6357f6240a1
CodeyHarsh <hv09989@gmail.com>
2025-03-30 11:28:33 +0530

    remove button fixed

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-
-function CartItem({id,title,price,quantity,img}) {
+import React from "react";
+import { Usecart } from "../contexts/CartProvider";
+function CartItem({ id, title, price, quantity, img }) {
+  const { increaseQty, DecreasesQty, Remove } = Usecart();
   return (
     <div className="cartItem">
       <p>id:{id}</p>
       <p>Title:{title}</p>
-      <p>Price:{price}</p>
+      <p>Price:{price * quantity}</p>
       <p>quantity:{quantity}</p>
       <p>img:{img}</p>
+      <button
+        onClick={() => {
+          return increaseQty(id);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          if (quantity <= 1) {
+            return;
+          }
+          return DecreasesQty(id);
+        }}
+      >
+        Decreases
+      </button>
+      <button
+        onClick={() => {
+          return Remove(id);
+        }}
+      >
+        Remove
+      </button>
     </div>
-  )
+  );
 }
 
-export default CartItem
+export default CartItem;
--- src/contexts/CartProvider.jsx
+++ src/contexts/CartProvider.jsx
@@ -1,24 +1,58 @@
 import { createContext, useContext, useReducer } from "react";
 function Cartreducer(cart, action) {
-switch(action.type){
-    case "Add_Item":{
-        return [...cart, action.payload];
+  switch (action.type) {
+    case "Add_Item": {
+      return [...cart, action.payload];
     }
-    
-}
+    case "increase_Qty": {
+      return cart.map((item) => {
+        if (item.id === action.payload.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        } else {
+          return item;
+        }
+      });
+    }
+    case "Decreases_Qty": {
+      return cart.map((item) => {
+        if (item.id === action.payload.id) {
+          return { ...item, quantity: item.quantity - 1 };
+        } else {
+          return item;
+        }
+      });
+    }
+    case "Remove_Qty": {
+      return cart.filter((item) => {
+       return item.id !== action.payload.id;
+      });
+    }
+  }
 }
 
 const Cartcontext = createContext();
 function CartProvider({ children }) {
   const [cart, dispatch] = useReducer(Cartreducer, []);
-  function CallDispatch(newCartItem){
-    dispatch({type:"Add_Item",payload:newCartItem})
+  function CallDispatch(newCartItem) {
+    dispatch({ type: "Add_Item", payload: newCartItem });
   }
+  const increaseQty = (id) => {
+    dispatch({ type: "increase_Qty", payload: { id: id } });
+  };
+  const DecreasesQty = (id) => {
+    dispatch({ type: "Decreases_Qty", payload: { id: id } });
+  };
+  const Remove = (id) => {
+    dispatch({ type: "Remove_Qty", payload: { id: id } });
+  };
   return (
     <Cartcontext.Provider
       value={{
         cart,
         CallDispatch,
+        increaseQty,
+        DecreasesQty,
+        Remove,
       }}
     >
       {children}
